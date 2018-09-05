fc2483a544dcfdbef3a357b27570396c1d677b1f
Jikku Jose <jikkujose@gmail.com>
2018-09-05 08:54:44 +0530

    Integrate react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import { Home } from "./Home"
+
+const NotFound = () => <div>Not Found</div>
 
 export default class App extends React.Component {
   render() {
-    return <Home />
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    )
   }
 }
