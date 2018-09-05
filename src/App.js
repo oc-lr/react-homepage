3921ec77f913ab0373f981e7553a1ff042535fee
Jikku Jose <jikkujose@gmail.com>
2018-09-05 18:04:53 +0530

    Fix basename properly

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { Home } from "./Home"
 
 const NotFound = () => <div>Not Found</div>
 
+const basename =
+  process.env["NODE_ENV"] === "production" ? "/react-homepage" : ""
+
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Router basename={basename}>
         <Switch>
-          <Route exact path="/react-homepage" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
