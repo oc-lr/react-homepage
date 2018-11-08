2b89cfec8cd931af627d94d8a0b500c959cbcd0b
Jikku Jose <jikkujose@gmail.com>
2018-11-08 13:01:15 +0530

    Savepoint

--- experiments/generic_ruby/lib/MortgageMachine.rb
+++ experiments/generic_ruby/lib/MortgageMachine.rb
@@ -1,9 +1,22 @@
 class MortgageMachine
-  def initialize(owner:)
+  def initialize(owner:, register: {})
     @owner = owner
+    @register = {}
   end
 
   def release_mortgage(initiator:, asset:)
+    if (asset_already_mortagaged?(asset))
+      @register.delete(asset)
+    else
+      fail 'Asset not mortagaged by the initiator'
+    end
+  end
+
+  def asset_already_mortagaged?(asset:)
+    @register.key?(asset)
+  end
+
+  def mortgage(initiator:, bank:, asset:)
 
   end
 end
